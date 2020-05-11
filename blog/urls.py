b5298a0a588ff53a991778d7b4e08f58fe08c154
AdrianIlovan <adrian88ilovan@yahoo.com>
2020-05-11 15:33:11 +0100

    Add fuature to create new blog posts from frontend.

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     # mydjangosite.com/post/2 -->online
     path("post/<int:pk>/", views.post_detail, name='post_detail'),
 
+    path('post/new/', views.post_new, name='post_new'),
+
 ]
