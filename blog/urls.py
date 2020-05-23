cc69f90fc257974c0da8534a16d7285a369bd149
AdrianIlovan <adrian88ilovan@yahoo.com>
2020-05-23 17:31:32 +0100

    Add feature to edit blog posts.

--- blog/urls.py
+++ blog/urls.py
@@ -11,6 +11,12 @@ urlpatterns = [
     # mydjangosite.com/post/2 -->online
     path("post/<int:pk>/", views.post_detail, name='post_detail'),
 
+    # 127.0.0.1:8000/post/new #or any number -->local
+    # mydjangosite.com/post/new -->online
     path('post/new/', views.post_new, name='post_new'),
+
+    # 127.0.0.1:8000/post/2/edit #or any number -->local
+    # mydjangosite.com/post/2/edit -->online
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 
 ]
