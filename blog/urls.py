80363c2c37a425216ff063e2621ef30c3b73ed28
AdrianIlovan <adrian88ilovan@yahoo.com>
2020-05-30 21:43:09 +0100

    Add login functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth.views import LoginView
+
 
 urlpatterns = [
     # 127.0.0.1:8000
@@ -25,5 +27,9 @@ urlpatterns = [
 
     # 127.0.0.1:8000/post/2/publish  -->local
     # mydjangosite.com/post/2/publish -->online
-    path('post/<int:pk>/publish/', views.post_publish, name='post_publish')
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+
+    # 127.0.0.1:8000/accounts/login  -->local
+    # mydjangosite.com/accounts/login -->online
+    path('accounts/login/', LoginView.as_view(), name='login'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-
+from django.contrib.auth.decorators import login_required
 from .models import Post
 from .forms import PostForm
 
@@ -18,6 +18,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', stuff_for_frontend)
 
 
+@login_required
 def post_new(request):
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -32,30 +33,34 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', stuff_for_frontend)
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
 
         # updating an existing form
         form = PostForm(request.POST, instance=post)
+
         if form.is_valid():
             post = form.save(commit=False)
-
+            post.author = request.user
 
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-        stuff_for_frontend = {'form': form}
+        stuff_for_frontend = {'form': form, 'post': post}
     return render(request, 'blog/post_edit.html', stuff_for_frontend)
 
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('-create_date')
     stuff_for_frontend = {'posts': posts}
     return render(request, 'blog/post_draft_list.html', stuff_for_frontend)
 
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.publish()
